4fde876c1f21223c382066c6b53e309fb6eb2977
Tim Welsh <tim@timwelsh.com>
2019-02-18 11:38:23 -0500

    song function working

--- liri.js
+++ liri.js
@@ -1,25 +1,38 @@
 #!javascript
+//The Requires
 require("dotenv").config();
 var fs = require("fs");
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
+var moment = require("moment");
+var axios = require("axios");
+
+//Global variables
 var arr = [];
 var queryString = "";
 var spotify = new Spotify(keys.spotify);
 var action = process.argv[2];
-// console.log(action);
+
+var args = process.argv;
+var inputArr = [];
+var queryString = "";
+
+for (var i = 3; i < args.length; i++) {
+    inputArr.push(args[i]);
+}
+queryString = inputArr.join(" ");
 
 switch (action) {
     case "concert-this":
-      concert();
+      concert(queryString);
       break;
     
     case "spotify-this-song":
-      song();
+      song(queryString);
       break;
     
     case "movie-this":
-      movie();
+      movie(queryString);
       break;
     
     case "do-what-it-says":
@@ -27,21 +40,39 @@ switch (action) {
       break;
     }
 
-    function song() {
+    function song(query) {
         fs.readFile("random.txt", "utf8", function(err, data) {
           if (err) {
             return console.log(err);
           }
-          // Break down all the words inside
-          data = data.split(",");
-          arr = data[1].split(" ");
-          for(var i = 0; i < arr.length; i++) {
-              var queryString = queryString + arr[i] + "+"
-          }
-          console.log(queryString);
-          
+          spotify.search({ type: 'track', query: query, limit: 1 }, function(err, data) {
+            if (err) {
+              return console.log('Error occurred: ' + err);
+            }
+          var results = data.tracks.items;
+          // console.log(data.tracks.items[0])
+          console.log("Song name is " + data.tracks.items[0].name); 
+          console.log("Album name is " + data.tracks.items[0].album.name)
+          console.log("Artist name is " + data.tracks.items[0].artists[0].name)
+          });
         });
       }
+
+    function concert() {
+      //TODO: concert function
+      console.log("concert");
+    }
+
+    function movie() {
+      //TODO: Movie function
+      console.log("movie");
+    }
+
+    function itSays() {
+      //TODO: random fucntion 
+      console.log("itSays");
+    }
+
 
 
 
