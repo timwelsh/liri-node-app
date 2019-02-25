4b66bfab28854a7755123ddd59183e96abe2d581
Tim Welsh <tim@timwelsh.com>
2019-02-24 21:16:48 -0500

    add assets

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # liri-node-app
+
+A command line node tool to query concerts, songs and movies.  
+
+![Alt text](assets/images/concert.PNG)
+
+![Alt text](assets/images/movie.PNG)
+
+![Alt text](assets/images/spotify.PNG)
+
+![Alt text](assets/images/do-what-it-says.PNG)
--- liri.js
+++ liri.js
@@ -33,6 +33,9 @@ switch (action) {
     }
 
     function song(queryString) {
+      if(queryString == "") {
+        queryString = "The Sign"
+      }
       spotify.search({ type: 'track', query: queryString, limit: 1, market: "US" }, function(err, data) {
         if (err) {
           return console.log(chalk.magenta('Error occurred: ' + err));
@@ -53,6 +56,7 @@ switch (action) {
           events.forEach(function (event) {
             console.log("Artist: " + queryString);
             console.log("Venue: " + event.venue.name);
+            // console.log(response.data[1])
             console.log("Venue Location: " + event.venue.city);
             console.log("\r\n");
           });
@@ -99,10 +103,9 @@ Actors: ${movies.Actors}`);
           lines.push(randomFile[i]);
         }
         let randomNum = Math.floor((Math.random() * lines.length - 1) + 1);
-        // console.log(randomNum + " here " + randomFile.length + " " + lines)
         line = lines[randomNum];
         randomLine = line.split(",")[0];
-        randomDomain = (line.split(",")[1].slice(1, -1))
+        randomDomain = (line.split(",")[1].slice(1, -1));
         randomRequest = randomDomain.split(" ").join("+");
         console.log(chalk.yellow(randomLine, randomDomain));
       })
